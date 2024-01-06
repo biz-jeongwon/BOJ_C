802adf104bae112a8a7357be3a3a6ae94e8b6337
biz-jeongwon <biz.jeongwon@gmail.com>
2024-01-06 18:19:53 +0900

    24.01.06_bojsolve2

--- 단계별로 풀어보기/13. 정렬/C_18870_좌표 압축/C_18870.c
+++ 단계별로 풀어보기/13. 정렬/C_18870_좌표 압축/C_18870.c
@@ -1,1 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	int val;
+    int org;
+} coord;
+
+int compare(const void* first, const void* second) 
+{
+	coord* f = (coord *)first;
+	coord* s = (coord *)second;
+
+	if (f->val > s->val)
+        return 1;
+	else if (f->val < s->val)
+        return -1;
+	else
+        return 0;
+}
+
+int main() 
+{
+	int N;
+
+	scanf("%d", &N);
+
+	coord arr[N];
+
+	for (int i = 0; i < N; i++) 
+    {
+		scanf("%d", &arr[i].val);
+		arr[i].org = i;
+	}
+
+	qsort(arr, N, sizeof(coord), compare);
+
+	int comp[N], cnt = -1, min = -1000000001;
+
+	for (int i = 0; i < N; i++) 
+    {
+		if (arr[i].val != min) 
+        {
+			min = arr[i].val;
+			cnt++;
+		}
+
+		comp[arr[i].org] = cnt;
+	}
+
+	for (int i = 0; i < N; i++) 
+		printf("%d ", comp[i]);
+}
+
